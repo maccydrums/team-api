c4bc951491bf0d3eb9ccbf247bf188292e221dc4
maccydrums <marcusprivat@live.com>
2019-05-24 17:29:37 +0200

    bug fix for mongoose

--- models/index.js
+++ models/index.js
@@ -7,6 +7,9 @@ const uri = process.env.DATABASE_URL || "mongodb://localhost:27017/schoolprojec"
 const connectDb = () => {
     return mongoose.connect(uri, {useFindAndModify: false});
 }
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useFindAndModify', true)
+mongoose.set('useCreateIndex', true)
 
 module.exports = {
     connectDb,
